f66ea882cf0d42f378a632a82a733bf0a69088c0
EmptyRamenBox <alexis.martinezo92@gmail.com>
2020-04-08 22:17:27 -0500

    Added Post Routes

--- src/api/posts.js
+++ src/api/posts.js
@@ -5,7 +5,21 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/posts", (req, res) => {
-  res.json({ msg: "Hello from Posts API" });
+  res.json({ todo: "Get ALL Posts" });
+});
+// Get post with Dynamic route
+router.get("/posts/:id", (req, res) => {
+  res.json({ todo: `Get one post with id: ${req.params.id}` });
+});
+router.post("/posts", (req, res) => {
+  res.json({ todo: "Add ONE Posts" });
+});
+router.put("/posts/:id", (req, res) => {
+  res.json({ todo: `Updating one post with id: ${req.params.id}` });
+});
+
+router.delete("/posts/:id", (req, res) => {
+  res.json({ todo: `Deleting one post with id: ${req.params.id}` });
 });
 
 export default router;
